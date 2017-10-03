eefd4c4d21002581c9d171f2053e0e8d9473c3b7
Sergey <sunliveua@gmail.com>
2017-10-03 10:33:10 +0300

    0.2.2

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,6 +1,26 @@
 <template>
   <footer>
-    
+    <v-container fluid>
+      <v-layout row wrap justify-center align-center>
+        <v-flex xs12 sm6 class="text-xs-center">
+          <ul>
+            <li><v-btn flat>Телефон</v-btn></li>
+            <li><v-btn flat>Состав</v-btn></li>
+            <li><v-btn flat>Описание</v-btn></li>
+            <li><v-btn flat>Преимущества</v-btn></li>
+            <li><v-btn flat>Галерея</v-btn></li>
+            <li><v-btn flat>О Нас</v-btn></li>
+            <li><v-btn flat>Заказать</v-btn></li>
+          </ul>
+        </v-flex>
+        <v-flex xs12 sm6 class="text-xs-center">
+          <h3>Контакты?</h3>
+          <h3>Телефоны?</h3>
+          <h3>Какая то информация?</h3>
+        </v-flex>
+      </v-layout>
+    </v-container>
+    <v-footer>InDresser &copy; {{ new Date().getFullYear() }}</v-footer>
   </footer>
 </template>
 
@@ -14,5 +34,7 @@
 </script>
 
 <style scoped>
-
+.footer {
+  justify-content: center;
+}
 </style>
